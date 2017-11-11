43fcb9df4ba31eab288a268a14d90252312d6b0a
Lucas <rukaso.mero@gmail.com>
2017-11-11 09:52:50 -0200

    first commit

---
+++ src/main/java/com/rukaso/starwars/shop/starwarsshop/StarwarsshopApplication.java
@@ -0,0 +1,12 @@
+package com.rukaso.starwars.shop.starwarsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsshopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rukaso/starwars/shop/starwarsshop/StarwarsshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rukaso.starwars.shop.starwarsshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarwarsshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
